f681b881e051d22aaeef5b40376f58fa6544c3d3
Auston Stewart <auston@shedbuilt.net>
2018-05-30 08:49:12 -1000

    Updating for blank

--- build.sh
+++ build.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-sed -i 's:\\\${:\\\$\\{:' intltool-update.in
-./configure --prefix=/usr
-make -j $SHED_NUM_JOBS
-make DESTDIR=${SHED_FAKE_ROOT} install
-install -v -Dm644 doc/I18N-HOWTO ${SHED_FAKE_ROOT}/usr/share/doc/intltool-0.51.0/I18N-HOWTO
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
+SHED_PKG_LOCAL_DOCDIR="/usr/share/doc/${SHED_PKG_NAME}-${SHED_PKG_VERSION}"
+# Patch an issue with Perl v5.22 and later
+sed -i 's:\\\${:\\\$\\{:' intltool-update.in &&
+# Configure
+./configure --prefix=/usr \
+            --docdir="$SHED_PKG_LOCAL_DOCDIR" &&
+# Build and Install
+make -j $SHED_NUM_JOBS &&
+make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
+# Install Documentation
+if [ -n "${SHED_PKG_LOCAL_OPTIONS[docs]}" ]; then
+    install -v -Dm644 doc/I18N-HOWTO "${SHED_FAKE_ROOT}${SHED_PKG_LOCAL_DOCDIR}/I18N-HOWTO" || exit 1
+else
+    rm -rf "${SHED_FAKE_ROOT}/usr/share/doc"
+fi
